1622a858f61d0664563d40eaa658739609dadf66
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-01-13 18:54:48 +0100

    Aggiunto scrollBar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,15 @@
-/*let cambiaColore =  () => {
-  let scroll_start = 0;
-  let startchange = $(".maintext");
-  let offset = startchange.offset();
+let scrollBar = window.scrollY;
+let nav = document.querySelector("nav");
+let button = document.querySelector(".buttonBGBlackNav");
 
-  if (startchange.length) {
-   let cambiaColore = () => {
-      scroll_start = $(this).scrollTop();
-      if (scroll_start > offset.top) {
-        $("nav").css("background-color", "#B5B5B5");
-      } else {
-        $("nav").css("background-color", "transparent");
-      }
-    });
+window.addEventListener("scroll", function () {
+  scrollBar = window.scrollY;
+
+  if (scrollBar >= 300) {
+    nav.classList.add("scrollBar");
+    button.classList.add("scrollButton");
+  } else {
+    nav.classList.remove("scrollBar");
+    button.classList.remove("scrollButton");
   }
-});*/
-
-addEventListener("scroll", (event) => {});
+});
