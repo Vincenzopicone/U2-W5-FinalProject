d22ca2ed5fba8943aee8737f4845cf4ea31fbd5c
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-01-13 16:36:26 +0100

    Fix Position Absolute svg e creato file JS

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+/*let cambiaColore =  () => {
+  let scroll_start = 0;
+  let startchange = $(".maintext");
+  let offset = startchange.offset();
+
+  if (startchange.length) {
+   let cambiaColore = () => {
+      scroll_start = $(this).scrollTop();
+      if (scroll_start > offset.top) {
+        $("nav").css("background-color", "#B5B5B5");
+      } else {
+        $("nav").css("background-color", "transparent");
+      }
+    });
+  }
+});*/
+
+addEventListener("scroll", (event) => {});
