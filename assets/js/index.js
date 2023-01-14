58ecefdd5a807831dda3a8f9e05eeee6818c845f
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-01-14 17:36:21 +0100

    Final FIX

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,12 @@
 let scrollBar = window.scrollY;
 let nav = document.querySelector("nav");
-let button = document.querySelector(".buttonBGBlackNav");
 
 window.addEventListener("scroll", function () {
   scrollBar = window.scrollY;
 
   if (scrollBar >= 300) {
     nav.classList.add("scrollBar");
-    button.classList.add("scrollButton");
   } else {
     nav.classList.remove("scrollBar");
-    button.classList.remove("scrollButton");
   }
 });
